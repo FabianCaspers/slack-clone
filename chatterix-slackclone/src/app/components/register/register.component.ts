26f30b666e22ee6ab9bc70a9b8c55ba37e61e69c
HansStieber <hans.stieber@posteo.de>
2023-05-07 11:25:41 +0200

    added Design for register

--- chatterix-slackclone/src/app/components/register/register.component.ts
+++ chatterix-slackclone/src/app/components/register/register.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  form!: FormGroup;
+  isRegistering = false;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      passwordRepeat: ['', [Validators.required]]
+    });
+  }
+
+
+  register() {
+    this.isRegistering = true;
+  }
 }
