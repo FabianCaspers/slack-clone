02553d6d938dd9c6ecb3da8924475779a40a4093
HansStieber <hans.stieber@posteo.de>
2023-05-07 11:49:34 +0200

    fixed bugs at login

--- chatterix-slackclone/src/app/components/login/login.component.ts
+++ chatterix-slackclone/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -9,7 +9,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   form!: FormGroup;
   isLoggingIn = false;
@@ -19,15 +19,12 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     private router: Router,
     private snackbar: MatSnackBar
-  ) { }
-
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required]]
+  ) {
+    this.form = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', Validators.required)
     });
   }
-
 
   login() {
     this.isLoggingIn = true;
