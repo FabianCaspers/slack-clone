016375ff02b3bafffbb2aa86c7d55492e412bde2
HansStieber <hans.stieber@posteo.de>
2023-05-07 11:07:40 +0200

    continued work at login not working yet

--- chatterix-slackclone/src/app/components/login/login.component.ts
+++ chatterix-slackclone/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +12,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form!: FormGroup;
+  isLoggingIn = false;
 
   constructor(
-    private formBuilder: FormBuilder
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackbar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['']
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
     });
+  }
+
+
+  login() {
+    this.isLoggingIn = true;
+
+    this.authenticationService.signIn({
+      email: this.form.value.email,
+      password: this.form.value.password
+    }).subscribe(() => {
+      this.router.navigate(['register']);
+    }, (error: any) => {
+      this.isLoggingIn = false;
+      this.snackbar.open(error.message, "OK", {
+        duration: 5000
+      })
+    })
   }
 }
