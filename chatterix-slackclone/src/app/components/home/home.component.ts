474b6c503049477809440609553b8d0b1138f103
FabianCaspers <fabian.c1308@icloud.com>
2023-05-09 18:39:33 +0200

    shows firstletter from first&lastname in organe box

--- chatterix-slackclone/src/app/components/home/home.component.ts
+++ chatterix-slackclone/src/app/components/home/home.component.ts
@@ -20,21 +20,32 @@ export class HomeComponent implements OnInit {
     private router: Router
   ) { }
 
-
+  // Read the firstname from logged user
   async ngOnInit() {
     this.authenticationService.getCurrenctUserCollection();
     await this.authenticationService.getCurrentUser();
     this.firstname = this.authenticationService.user.firstname;
   }
 
+  // Shows the firstletter of first + lastname on the orange profile box
+  getUserInitials(): string {
+    const loggedInUser = this.authenticationService.user;
+    if (loggedInUser) {
+      const firstnameInitial = loggedInUser.firstname.charAt(0).toUpperCase();
+      const lastnameInitial = loggedInUser.lastname.charAt(0).toUpperCase();
+      return firstnameInitial + lastnameInitial;
+    }
+    return '';
+  }  
 
+  // Logout function, goes back to login page
   logout() {
     this.authenticationService.logout().subscribe(() => {
       this.router.navigate(['login']);
     });
   }
 
-
+  // Change status
   onMenuItemClick(newText: string, button: HTMLElement): void {
     button.innerText = newText;
   }
