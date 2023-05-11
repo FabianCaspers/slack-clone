287b175d8712b85101c9d333a011fbcaf4fc6709
FabianCaspers <fabian.c1308@icloud.com>
2023-05-11 19:17:29 +0200

    create imprint

--- chatterix-slackclone/src/app/app-routing.module.ts
+++ chatterix-slackclone/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { ImprintComponent } from './components/imprint/imprint.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'imprint', component: ImprintComponent}
 ];
 
 @NgModule({
