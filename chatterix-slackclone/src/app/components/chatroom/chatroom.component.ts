ec7166558c1a60c6d2c530aa9e1917bad0ec42be
FabianCaspers <fabian.c1308@icloud.com>
2023-06-12 19:01:22 +0200

    style chatarea

--- chatterix-slackclone/src/app/components/chatroom/chatroom.component.ts
+++ chatterix-slackclone/src/app/components/chatroom/chatroom.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { DeleteChannelDialogComponent } from 'src/app/dialogs/delete-channel-dialog/delete-channel-dialog.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-
+import { DmChannelService } from 'src/app/services/dm-channel.service';
 
 @Component({
   selector: 'app-chatroom',
@@ -24,6 +24,7 @@ export class ChatroomComponent {
     private firestore: AngularFirestore,
     private dialog: MatDialog,
     public channelService: ChannelService,
+    public dmChannelService: DmChannelService,
     public authenticationService: AuthenticationService,
   ) { }
 
@@ -35,6 +36,15 @@ export class ChatroomComponent {
     });
   }
 
+  getUserOnlineStatus(): string {
+    const loggedInUser = this.authenticationService.loggedInUserFromDb;
+    if (loggedInUser) {
+      console.log(loggedInUser)
+      const color = loggedInUser['onlineStatus'];
+      return color;
+    }
+    return '';
+  }
 
   openDeleteChannelDialog() {
     this.dialog.open(DeleteChannelDialogComponent);
@@ -47,5 +57,15 @@ export class ChatroomComponent {
       return firstname;
     }
     return '';
+  }
+  getUserInitialsById(otherUserId: String) {
+    const user = this.authenticationService.users.find(obj => obj['userId'] === otherUserId);
+    if (user) {
+      const firstLeter = user['firstname'].charAt(0).toUpperCase();
+      const lastLetter = user['lastname'].charAt(0).toUpperCase();
+      return firstLeter + lastLetter;
+    } else {
+      throw new Error('Benutzer nicht gefunden');
+    }
   }
 }
