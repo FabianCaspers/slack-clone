0cc7f7bd15886fb20a1269bb61bca9d962d10b7f
HansStieber <hans.stieber@posteo.de>
2023-06-26 15:34:40 +0200

    fixed search input position

--- chatterix-slackclone/src/app/components/home/home.component.ts
+++ chatterix-slackclone/src/app/components/home/home.component.ts
@@ -43,7 +43,6 @@ export class HomeComponent implements OnInit, OnDestroy {
           this.drawer.close();
         }
       });
-      this.searchAllMessages.nativeElement.style.display = 'none';
     }
   }
 
